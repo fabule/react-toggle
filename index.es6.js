affeb62284953f6cfc7fc7a84fbd48efaea7e863
Bruno Nadeau <bruno@fabule.com>
2015-07-26 18:46:37 -0400

    Started adding checked and unchecked labels. Removed linter errors.

--- index.es6.js
+++ index.es6.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import classNames from 'classnames'
-import Check from './check'
-import X from './x'
-import {addons} from 'react/addons'
+'use strict';
+
+import React from 'react';
+import classNames from 'classnames';
+import Check from './check';
+import X from './x';
+import {addons} from 'react/addons';
 
 var PureRenderMixin = addons.PureRenderMixin;
 
@@ -19,49 +21,51 @@ export default React.createClass({
     value: React.PropTypes.string,
     id: React.PropTypes.string,
     'aria-labelledby': React.PropTypes.string,
-    'aria-label': React.PropTypes.string
+    'aria-label': React.PropTypes.string,
+    checkedLabel: React.PropTypes.string,
+    uncheckedLabel: React.PropTypes.string
   },
 
   getInitialState() {
     var checked = false;
     if ('checked' in this.props) {
-      checked = this.props.checked
+      checked = this.props.checked;
     } else if ('defaultChecked' in this.props) {
-      checked = this.props.defaultChecked
+      checked = this.props.defaultChecked;
     }
     return {
       checked: !!checked,
       hasFocus: false
-    }
+    };
   },
 
   componentWillReceiveProps(nextProps) {
     if ('checked' in nextProps) {
-      this.setState({checked: !!nextProps.checked})
+      this.setState({checked: !!nextProps.checked});
     }
   },
 
   handleClick(event) {
-    var checkbox = React.findDOMNode(this.refs.input)
+    var checkbox = React.findDOMNode(this.refs.input);
     if (event.target !== checkbox)
     {
-      event.preventDefault()
-      checkbox.focus()
-      checkbox.click()
-      return
+      event.preventDefault();
+      checkbox.focus();
+      checkbox.click();
+      return;
     }
 
     if (!('checked' in this.props)) {
-      this.setState({checked: checkbox.checked})
+      this.setState({checked: checkbox.checked});
     }
   },
 
   handleFocus() {
-    this.setState({hasFocus: true})
+    this.setState({hasFocus: true});
   },
 
   handleBlur() {
-    this.setState({hasFocus: false})
+    this.setState({hasFocus: false});
   },
 
   render() {
@@ -69,26 +73,26 @@ export default React.createClass({
       'react-toggle--checked': this.state.checked,
       'react-toggle--focus': this.state.hasFocus,
       'react-toggle--disabled': this.props.disabled
-    })
+    });
 
     return (
       <div className={classes} onClick={this.handleClick}>
-        <div className="react-toggle-track">
-          <div className="react-toggle-track-check">
+        <div className='react-toggle-track'>
+          <div className='react-toggle-track-check'>
             <Check />
           </div>
-          <div className="react-toggle-track-x">
+          <div className='react-toggle-track-x'>
             <X />
           </div>
         </div>
-        <div className="react-toggle-thumb"></div>
+        <div className='react-toggle-thumb'></div>
 
         <input
-          ref="input"
+          ref='input'
           onFocus={this.handleFocus}
           onBlur={this.handleBlur}
-          className="react-toggle-screenreader-only"
-          type="checkbox"
+          className='react-toggle-screenreader-only'
+          type='checkbox'
           {...this.props} />
       </div>
     )
